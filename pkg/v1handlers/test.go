188b983c824520e9ad02ac8604e10da6dbce0145
Yuqing Chen <chenyuqing@caicloud.io>
2018-09-21 19:55:02 +0800

    test list files

--- pkg/v1handlers/test.go
+++ pkg/v1handlers/test.go
@@ -1,11 +1,26 @@
 package v1handlers
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
+var _ = fmt.Print // this statement is used only when DEBUGGING
+
 func Test(c *gin.Context) {
+	files, err := ioutil.ReadDir("/home/matt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var fileNames []string
+	for _, file := range files {
+		// fmt.Println("file is", file.Name())
+		fileNames = append(fileNames, file.Name())
+	}
 	c.JSON(200, gin.H{
-		"message": "hello rmdashrf!",
+		"files": fileNames,
 	})
 }
