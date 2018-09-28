b26bef90a85a19da886da6c2de11ea1d14ada9aa
Yuqing CHEN <chenyuqing@caicloud.io>
2018-09-28 21:29:13 +0800

    wip: download dir

--- pkg/v1handlers/download.go
+++ pkg/v1handlers/download.go
@@ -1,6 +1,7 @@
 package v1handlers
 
 import (
+	"archive/zip"
 	"log"
 	"net/http"
 	"os"
@@ -53,12 +54,38 @@ func DownloadDirAsZip(c *gin.Context) {
 		return
 	}
 
+	// buf := new(bytes.Buffer)
+	zipWriter := zip.NewWriter(c.Writer)
+	// const testFileName = "/home/matt/Projects/github.com/yuqingc/data/package.json"
+	// testFileData, err := ioutil.ReadFile(testFileName)
+	const testFileData = "hello world\n"
+	if err != nil {
+		log.Println(err)
+		c.String(http.StatusBadRequest, err.Error())
+	}
+	f, err := zipWriter.Create("haha/hahaha.txt")
+	if err != nil {
+		log.Println(err)
+		c.String(http.StatusBadRequest, err.Error())
+	}
+	_, err = f.Write([]byte(testFileData))
+	_, err = f.Write([]byte(testFileData))
+	if err != nil {
+		log.Println(err)
+		c.String(http.StatusBadRequest, err.Error())
+	}
+
 	// TODO create and write zip to stream
 	// TODO: open download window, maybe not file download
-	c.Header("Content-Disposition", "attachment; filename=WHATEVER_YOU_WANT")
+	c.Header("Content-Disposition", "attachment; filename=aaa.zip")
 	c.Header("Content-Type", "application/x-zip-compressed")
 	// This will be a for loop to write zip file
 	// generate and write
-	c.Writer.Write([]byte("hello"))
+	// c.Writer.Write([]byte("hello"))
+	// io.Copy(c.Writer, buf)
+	err = zipWriter.Close()
+	if err != nil {
+		log.Println(err)
+	}
 
 }
