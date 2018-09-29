4eb1aee92c32beb3b7d6b3c9096c43db96ddca22
Yuqing CHEN <chenyuqing@caicloud.io>
2018-09-29 12:06:03 +0800

    performace zipping process

--- pkg/manager/zip.go
+++ pkg/manager/zip.go
@@ -38,28 +38,50 @@ func walkAndZip(src string, dst string, zipWriter *zip.Writer) (err error) {
 				return err
 			}
 		} else {
-			buf := make([]byte, 1024)
-			originFile, err := os.Open(contentPath)
-			if err != nil {
-				return err
-			}
-			defer originFile.Close()
-			fileWriter, err := zipWriter.Create(dst + contentName) // for a file
-			for {                                                  // read and write
-				nr, err := originFile.Read(buf)
-				if nr < 0 {
-					return err
-				} else if nr == 0 { // EOF
-					break // Do not use switch-case or break won't go out of the loop
-				} else {
-					nw, err := fileWriter.Write(buf[0:nr])
-					if nw != nr {
-						return fmt.Errorf("write buff length is not equal to read buff")
-					}
-					if err != nil && err != io.EOF {
-						return err
+			errChan := make(chan error)
+			// In order to  close the file immediately it is read completely,
+			// a goroutine is created. Otherwise the opened file will not be closed
+			// until the outer function `walkAndZip` exits (by `defer originFile.Close()`).
+			// This could cause a lot file descriptors remain in the kernel.
+			// A new goroutine might not be necessary,
+			// because We can also use a function which returns `error` here
+			go func() {
+				buf := make([]byte, 1024)
+				originFile, err := os.Open(contentPath)
+				if err != nil {
+					errChan <- err
+					return
+				}
+				// `defer` is necessary here.
+				// Do not put the Close expression after the for loop,
+				// or the file will never be closed when there is an error,
+				// and the Close expression will never be reached
+				defer originFile.Close()
+				fileWriter, err := zipWriter.Create(dst + contentName) // for a file
+				for {                                                  // read and write
+					nr, err := originFile.Read(buf)
+					if nr < 0 {
+						errChan <- err
+						return
+					} else if nr == 0 { // EOF
+						break // Do not use switch-case or break won't go out of the loop
+					} else {
+						nw, err := fileWriter.Write(buf[0:nr])
+						if nw != nr {
+							errChan <- fmt.Errorf("write buff length is not equal to read buff")
+							return
+						}
+						if err != nil && err != io.EOF {
+							errChan <- err
+							return
+						}
 					}
 				}
+				errChan <- nil
+			}()
+			err = <-errChan
+			if err != nil {
+				return
 			}
 		}
 	}
